f7b3a8c6d686ef7d72993e05be6612ec59858620
Yevgenia <shaparenkoevgenia@gmail.com>
2021-11-26 12:56:13 +0200

    add script by show on scroll modal

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//for products count
 let productsCountEl = document.querySelector(".product-count");
 console.log(productsCountEl);
 
@@ -9,10 +10,49 @@ addToCard.forEach((item) =>
         productsCountEl.textContent = +productsCountEl.textContent + 1;
     })
 );
-
+//for likes
 let likes = document.querySelectorAll(".like");
 likes.forEach((item) => 
     (item).addEventListener("click", (event) => {
       event.target.classList.toggle("likes");
     })
 );
+//for modal and form
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+let modal = document.querySelector(".modal");
+let btnClose = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+btnClose.addEventListener("click", closeModal);
+
+
+function openModal() {
+modal.classList.add("show") ;
+modal.classList.remove("hide");
+};
+
+function closeModal() {
+modal.classList.add("hide");
+modal.classList.remove("show");
+};
+
+modal.addEventListener("click", function (e) {
+if (e.target === modal) {
+closeModal();
+}
+});
+//for modal on scroll
+
+ function showModalonScroll() {
+    let midHeight = document.documentElement.offsetHeight / 2;
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrolled > midHeight){
+        openModal();
+        window.removeEventListener("scroll",showModalonScroll);
+    }else{
+        closeModal();
+    };
+  };
+  window.addEventListener("scroll",showModalonScroll);
+
+  
